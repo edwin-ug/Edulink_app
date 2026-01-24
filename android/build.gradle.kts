256ef02a3e6830dcc2c80e3eac97e7662a327acc
edwin-ug <edwinkomagum@gmail.com>
2026-01-24 14:47:23 +0300

    feat: Update iOS and macOS project files for Firebase integration
    - Updated Main.storyboard to use the latest tools version and added device specifications.
    - Modified Info.plist to include new keys for frame duration and indirect input events support.
    - Registered cloud_firestore and firebase_auth plugins in macOS project.
    - Updated project.pbxproj to include new Pods and their configurations.
    - Added Pods.xcodeproj to the workspace for better dependency management.
    - Updated pubspec.yaml and pubspec.lock to include cloud_firestore and firebase_auth dependencies.
    - Updated widget test to reflect the new app name.
    - Registered Firebase plugins in Windows generated files.
    - Created Podfile.lock for iOS and macOS to manage CocoaPods dependencies.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,20 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // 1. Android Tools (Required)
+        classpath("com.android.tools.build:gradle:8.3.0") 
+        
+        // 2. Kotlin (Required)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0") 
+        
+        // 3. FIREBASE GOOGLE SERVICES (The missing link!)
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,16 +22,14 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
